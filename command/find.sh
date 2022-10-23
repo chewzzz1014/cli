9adb24e114c29d8d7ca3c73567057a3dfa834f27
chewzzz <chewziqing@gmail.com>
2022-10-23 20:28:53 +0800

    Add more options of find command"

--- command/find.sh
+++ command/find.sh
@@ -14,6 +14,32 @@ find . -type f -iname "*.sh"
 
 # find files modified in less than 10 minutes
 # - : less than
-# + : for than
+# + : more than
 find . -type f -mmin -10
 
+# > 1 min ago and less than 5 min ago
+find . -type f -mmin +1 -mmin -5
+
+
+# find files modified in more than 20 days
+find. -type f -mtime +20
+
+
+# find files of more than 5 MB
+# can also use 'k' for KB and 'G' for GB
+find . -size +5M
+
+
+# find empty files in dir
+find . -empty
+
+# find files by permission
+find . -perm 777
+
+
+#  execute command on selected files
+find [path] -exec [command]
+
+# use {} as placeholder for the result found
+# end with +
+find my_folder -type d -exec chmod 775 {} +

46fc6702ba5521e6d76fbc2b9b5e1cd65bb8d5e5
chewzzz <chewziqing@gmail.com>
2022-11-01 20:51:45 +0800

    Add wc and nl command

---
+++ abt-text/wc-nl.sh
@@ -0,0 +1,14 @@
+# wc: word count, show total count of words in a file 
+# display num of lines, num of words and num of bytes
+wc peanuts
+
+# -l: get num of lines only
+# -w: get num of words only
+# -c: get num of bytes only
+wc -c peanuts
+
+# nl: number lines, check the count of lines in a file 
+# eg. 
+# 1. ... 
+# 2. ...
+nl peanuts

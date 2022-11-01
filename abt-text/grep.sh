2380a6ef84a1387c90f430b1e145a558d75bcad0
chewzzz <chewziqing@gmail.com>
2022-11-01 20:57:12 +0800

    Add grep command

---
+++ abt-text/grep.sh
@@ -0,0 +1,9 @@
+# search files for characters that match a certain pattern
+#  return the line that have the pattern
+grep hello peanuts.txt
+
+# case insensitive
+grep -i HeLlO peanuts.txt
+
+# usong regex
+grep "*.sh" ../

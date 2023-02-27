81b60241675a95b1b5535cddf75f191248056daf
chewzzz <chewziqing@gmail.com>
2023-02-27 14:15:20 +0800

    Start exercise chap 2

---
+++ exercises/chapter-2.sh
@@ -0,0 +1,27 @@
+# 1. Copy a file from one directory to another.
+# 2. Move a file from one directory to another.
+# 3. Rename a file in the same directory.
+# 4. Copy a directory and all of its contents to another location.
+# 5. Move a directory and all of its contents to another location.
+# 6. Rename a directory in the same location.
+# 7. Copy multiple files to another directory.
+# 8. Move multiple files to another directory.
+# 9. Copy files with specific extensions to another directory.
+# 10. Move files with specific extensions to another directory.
+# 11. Rename multiple files in the same directory.
+# 12. Copy a file to a directory using an absolute path.
+# 13. Move a file to a directory using an absolute path.
+# 14. Copy a directory to another directory using a relative path.
+# 15. Move a directory to another directory using a relative path.
+# 16. Copy a file to the home directory.
+# 17. Move a file to the home directory.
+# 18. Copy a directory to the root directory.
+# 19. Move a directory to the root directory.
+# 20. Rename a file to a new name with spaces in it.
+# 21. Create a new file named “permission_file.txt” and change its permissions to read-only for the
+# owner, and no access for others.
+# 22. Create a new directory named “permission_dir” and change its permissions to allow the owner
+# to read, write, and execute, and no access for others.
+# 23. Add write permission to the group owner of “permission_file.txt”.
+# 24. Add execute permission to others for “permission_dir”.
+# 25. Change the owner of “permission_file.txt” to another user and verify the change.
